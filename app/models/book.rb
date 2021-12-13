d8f98a6dffa353d1ca203ac193821d2450696fa9
vietnguyen <nguyencongvieth@gmail.com>
2021-12-13 06:59:18 +0000

    review book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
     has_one_attached :cover
     has_many :chapter, -> { order('chapter_order DESC') }
+    has_many :book_review, -> { order "created_at DESC"}
     validates :title, :description, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :book_reviews
   get 'home/index'
   resources :books
   resources :chapters
